16f5194243a40d421ab12fd6a1f3e6069cd13ea3
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-03-05 12:06:55 -0300

    Add model Seller.

--- store/models.py
+++ store/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Seller(models.Model):
+    fantasy_name = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=200)
+    cnpj = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(max_length=150)
+    phone = models.CharField(max_length=15)
+
+

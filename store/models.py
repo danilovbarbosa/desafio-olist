dd1a4f616b54e0ca0fa3f2500efbefea1263cc88
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-03-07 15:18:30 -0300

    Add TODO em class Address.

--- store/models.py
+++ store/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+class Address(models.Model):
+    '''
+    TODO: to implement complete address;
+    '''
+    country = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    street = models.CharField(max_length=200)
+    number = models.CharField(max_length=20)
+    zip_code = models.CharField(max_length=10)
+
 class Seller(models.Model):
     fantasy_name = models.CharField(max_length=200)
     company_name = models.CharField(max_length=200)

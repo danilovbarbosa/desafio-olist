16e181c1eb86ad563ec4456b0db9acc943f59f1f
Danilo Costa <danilo.barbosa@fapce.edu.br>
2021-03-08 11:36:20 -0300

    Index template showing all products.

--- store/views.py
+++ store/views.py
@@ -1,5 +1,18 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from store.models import Product
+
+
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.order_by('name')
+    paginator = Paginator(products, 10)
+    page = request.GET.get('page')
+    products_per_page = paginator.get_page(page)
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'index.html', context=context)
